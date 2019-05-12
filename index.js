639012aecfc9e8a725d2d912577958c8f3692999
corey chang <coreychang808@hotmail.com>
2019-05-12 00:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()) {
-    return 'YES INDEED!';
-  } else {
-   return "I can't hear you!"; 
-  }
+  
   if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
