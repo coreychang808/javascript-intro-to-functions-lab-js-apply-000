ca050480b992b2c8991a04680c1a56eb3e57a8ed
corey chang <coreychang808@hotmail.com>
2019-05-12 00:12:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function shout(string) {
+  console.log('This is the shout function.')
   return string.toUpperCase();
 }
 
 function whisper(string) {
+  console.log('This is the whisper function with this variable ' + string)
   return string.toLowerCase();
 }
 
